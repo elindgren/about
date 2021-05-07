fb01ada9c08fbdf3d18e0a729f36e66ee136d279
Eric <ericlin@student.chalmers.se>
2021-05-07 22:50:20 +0200

    Begin with initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Button, Grid, Typography, Paper } from '@material-ui/core';
 import './App.css';
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#382E61", 
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+ 
+
 function App() {
+  const classes = useStyles();
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className={classes.root}>
+      <Grid container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Hi and Welcome!</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>This is my (WIP) GH page</Paper>
+        </Grid>
+      </Grid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
