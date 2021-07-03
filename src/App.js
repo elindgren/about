b629a10cc778f4078371b6f921464f0370ad0d63
Eric <ericlin@student.chalmers.se>
2021-07-03 19:09:20 +0200

    Continue working on about page layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Button, Grid, Typography, Paper } from '@material-ui/core';
+import { Box, Button, Grid, Typography, Paper, Avatar } from '@material-ui/core';
 import './App.css';
 
 
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    width: '90%',
   },
 }));
 
@@ -30,13 +30,25 @@ function App() {
         direction="column"
         alignItems="center"
         justify="center"
-        style={{ minHeight: '100vh' }}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>Hi and Welcome!</Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>This is my (WIP) GH page</Paper>
-        </Grid>
+        style={{ minHeight: '100vh' }}
+        >
+        <Paper className={classes.paper}>
+          <Grid direction="column" spacing={2} alignItems="flex-start" item xs={12}>
+            <Typography variant="h6" >2020-2021</Typography>
+            <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+              
+              <Grid item>
+                <Avatar>W</Avatar>
+              </Grid>
+              <Grid item>
+                <Typography >This is my WIP GH page</Typography>
+              </Grid>
+              <Grid item>
+                <Typography color="textSecondary" variant="subtitle1">2020-2021</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
     </div>
   )
